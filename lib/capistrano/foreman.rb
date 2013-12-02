8736656394928839a38acb1b2225e6361e3a037f
Joshua Samberg <josh@shelby.tv>
2013-12-02 17:26:37 -0500

    Ensure sudo password can always be entered when needed

--- lib/capistrano/foreman.rb
+++ lib/capistrano/foreman.rb
@@ -9,8 +9,8 @@ Capistrano::Configuration.instance(:must_exist).load do |configuration|
     desc "Export the Procfile to Ubuntu's upstart scripts"
     task :export, roles: :app do
       cmd = foreman_use_binstubs ? 'bin/foreman' : 'bundle exec foreman'
-      run "if [[ -d #{foreman_upstart_path} ]]; then #{foreman_sudo} mkdir -p #{foreman_upstart_path}; fi"
-      run "cd #{release_path} && #{foreman_sudo} #{cmd} export upstart #{foreman_upstart_path} #{format(options)}"
+      run "if [[ -d #{foreman_upstart_path} ]]; then #{foreman_sudo} -p 'sudo password: ' mkdir -p #{foreman_upstart_path}; fi"
+      run "cd #{release_path} && #{foreman_sudo} -p 'sudo password: ' #{cmd} export upstart #{foreman_upstart_path} #{format(options)}"
     end
 
     desc "Start the application services"
@@ -25,7 +25,7 @@ Capistrano::Configuration.instance(:must_exist).load do |configuration|
 
     desc "Restart the application services"
     task :restart, roles: :app do
-      run "sudo service #{options[:app]} start || sudo service #{options[:app]}  restart"
+      run "sudo -p 'sudo password: ' service #{options[:app]} start || sudo -p 'sudo password: ' service #{options[:app]} restart"
     end
 
     def options
@@ -40,5 +40,5 @@ Capistrano::Configuration.instance(:must_exist).load do |configuration|
       opts.map { |opt, value| "--#{opt}='#{value}'" }.join " "
     end
   end
-  
+
 end
